bad6382aab1e02d0566c4a798fc39097a9e45e06
Simone Sturniolo <simonesturniolo@gmail.com>
2026-05-01 14:49:20 +0100

    feat: wire index.ts exports, add integration test

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+export { initParser, parse } from './src/parser.ts'
+export { getDiagnostics } from './src/diagnostics.ts'
+export { getHighlights } from './src/highlights.ts'
+export { getFolding } from './src/folding.ts'
+export { getCompletions } from './src/completions.ts'
+export { getHover } from './src/hover.ts'
+export { getDefinition } from './src/definition.ts'
+export { preprocess, ImportError, CircularImportError } from './src/preprocess.ts'
+export type {
+  Language,
+  Tree,
+  SyntaxNode,
+  FileResolver,
+  Position,
+  Range,
+  HighlightRange,
+  DiagnosticSeverity,
+  Diagnostic,
+  CompletionItemKind,
+  CompletionItem,
+  HoverResult,
+  FoldingRange,
+  Location,
+  PreprocessWarning,
+  PreprocessResult,
+} from './src/types.ts'
+export { DiagnosticSeverity, CompletionItemKind } from './src/types.ts'
+export { KEYWORD_REGISTRY, KEYWORD_NAMES, RESERVED_KEYWORDS } from './src/keywords.ts'
